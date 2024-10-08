16e7bc0fa2b20c5439cd1349a230cd153af4e77e
benjamin <benjaminverlaine@gmail.com>
2024-10-08 09:18:59 +0200

    WIP integration de la maquette

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
-export default function App() {
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Footer from "./page/Footer/Footer";
+import HeroHeader from "./page/HeroHeader/HeroHeader";
+import Login from "./page/Login/Login";
+import NavBar from "./page/NavBar/NavBar";
+import PageError from "./page/PageError/PageError";
+import User from "./page/User/User";
+
+// Layout qui contient le NavBar et Footer, et l'Outlet pour les enfants
+function Layout() {
     return (
-        <>
-            <h1>test</h1>
-        </>
+        <body>
+            <NavBar />
+            <Outlet />{" "}
+            {/* C'est ici que le contenu des routes enfants sera rendu */}
+            <Footer />
+        </body>
     );
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />, // Layout qui inclut NavBar, Outlet et Footer
+        children: [
+            {
+                path: "/",
+                element: <HeroHeader />, // Affiche HeroHeader à la racine "/"
+            },
+            {
+                path: "login",
+                element: <Login />, // Affiche Login à "/login"
+            },
+            {
+                path: "user",
+                element: <User />, // Affiche User à "/user"
+            },
+            {
+                path: "*",
+                element: <PageError />, // Affiche PageError pour toutes les autres routes
+            },
+        ],
+    },
+]);
+
+export default function App() {
+    return <RouterProvider router={router} />;
 }
---
+++ src/page/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+export default function NavBar() {
+    return (
+        <nav className="main-nav">
+            <Link className="main-nav-logo" to="/">
+                <img
+                    className="main-nav-logo-image"
+                    src="src/assets/img/argentBankLogo.png"
+                    alt="Argent Bank Logo"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </Link>
+            <div>
+                <Link className="main-nav-item" to="/login">
+                    <i className="fa fa-user-circle"></i>
+                    Sign In
+                </Link>
+            </div>
+        </nav>
+    );
+}
